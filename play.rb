3f01a3c1658d426728d2fc2390f45fa41829ef9d
Polly Sileo <pollysileo@gmail.com>
2015-05-24 11:54:01 -0400

    added tic-tac-toe file to run the game

--- play.rb
+++ play.rb
@@ -24,27 +24,4 @@ class Player
 
 end
 
-p = Player.new
 
-game = TicTacToeGame.new
-
-system("clear")
-game.display_board
-
-until game.over?
-  #binding.pry
-  game.update_board p, p.get_player_move
-  system("clear")
-  #binding.pry
-  game.display_board
-  p.switch_player
-end
-
-
-if game.won?
-
-  puts "Game Over, Player #{p.switch_player} is the winner!"
-
-elsif game.tie?
-  puts "No more moves available - This game ends in a TIE"
-end
